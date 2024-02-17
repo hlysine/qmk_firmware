0c18890093641b64616332068dffd9a686eef23c
Henry Lin <henry.ys.lin@gmail.com>
2024-02-17 16:34:13 +0800

    Swap layers 1 and 2 and change layer switch accordingly

--- keyboards/keychron/k3_max/k3_max.c
+++ keyboards/keychron/k3_max/k3_max.c
@@ -34,7 +34,7 @@ static uint32_t power_on_indicator_timer;
 
 bool dip_switch_update_kb(uint8_t index, bool active) {
     if (index == 0) {
-        default_layer_set(1UL << (active ? 0 : 2));
+        default_layer_set(1UL << (active ? 0 : 1));
     }
     dip_switch_update_user(index, active);
 
